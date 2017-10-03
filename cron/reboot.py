74f08e3938ea09b55a3fb8a1d1c66ac8e9cef657
tomasz2101 <tmk.witkowski2@gmail.com>
2017-10-03 18:35:39 +0200

    Main fixes, adding led and lights

--- cron/reboot.py
+++ cron/reboot.py
@@ -16,16 +16,16 @@ file = Path("/skateboard/log/cron_" + strftime("%Y_%m_%d", gmtime()) + ".txt")
 
 
 def main():
-    log_write(12, "Starting " + configuration["environment"] + " mode.")
+    # log_write(12, "Starting " + configuration["environment"] + " mode.")
     try:
         subprocess.call(["sudo", "killall", "pigpiod"])
         subprocess.call(["sudo", "pigpiod"])
-        log_write(16, "sudo pigpiod executed")
+        # log_write(16, "sudo pigpiod executed")
     except OSError as e:
         debug_write(18, "sudo pigpiod failed: (" + configuration["environment"] + ") " + str(e))
     try:
         subprocess.call(["python", "main.py"], cwd="/skateboard/src")
-        log_write(21, "sudo python start_up.py executed")
+        # log_write(21, "sudo python start_up.py executed")
     except OSError as e:
         debug_write(23, "sudo python start_up.py failed: " + str(e))
 
